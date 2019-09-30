6a4c38bfff1bfe4758c1d368b90581a67e7d5009
Sushant Kumar <36587557+sushkr34@users.noreply.github.com>
2019-09-30 03:39:59 -0700

    Update index.js

--- src/Components/Components/Flexi/index.js
+++ src/Components/Components/Flexi/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Button from '../Button'
-import Card from '../Card'
-import Cards from '../Cards'
-import RadioGroup from '../RadioGroup'
-import Header from '../Header'
-import TextField from '../TextField'
+// import Button from '../Button'
+// import Card from '../Card'
+// import Cards from '../Cards'
+// import RadioGroup from '../RadioGroup'
+// import Header from '../Header'
+// import TextField from '../TextField'
 
 const Flexi =({config,handleChange, onFlexiSubmit })=>{
     let items = config.items;
@@ -21,27 +21,37 @@ const Flexi =({config,handleChange, onFlexiSubmit })=>{
          if(e.children){
              child =getChild(e.children.items);
          }
+         let Components = require ('../${e.type}').default
+         return (
+             <Components
+             props={e.props}
+             child={child}
+             handleChange={handleChange}
+             onFlexiSubmit={onFlexiSubmit}
+             />
+
+         )
              
-             switch (e.type){
-                 case "Card":
+            //  switch (e.type){
+            //      case "Card":
                      
-                     return <Card props ={e.props} child={child} />;
+            //          return <Card props ={e.props} child={child} />;
                        
-                case "Cards" :
-                        return <Cards props = {e.props}/>;
+            //     case "Cards" :
+            //             return <Cards props = {e.props}/>;
                         
-                case "Header" :
-                    return <Header props = {e.props}/>;
+            //     case "Header" :
+            //         return <Header props = {e.props}/>;
                         
-                case "TextField" :
-                    return <TextField props = {e.props} handleChange={handleChange}/>
-                case "Button" :
-                    return <Button props ={e.props} onFlexiSubmit={onFlexiSubmit}/>
-                case "RadioGroup" :
-                    return <RadioGroup props= {e.props} handleChange={handleChange}/>
-                default :
-                    return null;
-             }
+            //     case "TextField" :
+            //         return <TextField props = {e.props} handleChange={handleChange}/>
+            //     case "Button" :
+            //         return <Button props ={e.props} onFlexiSubmit={onFlexiSubmit}/>
+            //     case "RadioGroup" :
+            //         return <RadioGroup props= {e.props} handleChange={handleChange}/>
+            //     default :
+            //         return null;
+            //  }
          
      }
      return <div>{getChild (items )}</div>
